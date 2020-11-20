e5e2b8e312e5d61092686c164ee107672fc8a65c
J3T4R0 <niemiryan@gmail.com>
2020-11-19 18:27:42 -0800

    simulates random button presses of 1 to 5 with color

--- button/test/setup.py
+++ button/test/setup.py
@@ -1,41 +1,52 @@
 import time
+import random
 from setup import led, rgb, check
 
 class State:
     def __init__(self):
-        self.led = False
         self.button_a = False
         self.button_b = False
+        self.led = False
         self.rgb = False
         self.color = (255, 255, 255)
 
+    def random_color(self):
+        self.color = (
+             random.randrange(0, 255),
+             random.randrange(0, 255),
+             random.randrange(0, 255)
+        )
+
     def __repr__(self):
-        return "<Buttons: {}/{}, LED: {}, RGB: {}, Color: {}>".format(self.button_a, self.button_b, self.led, self.rgb, self.color)
+        return "<Buttons: {}/{}, LED: {}, Color: {}>".format(self.button_a, self.button_b, self.led, self.color)
 
 state = State()
 
 while True:
-    # first pass: check real life
+    # update the state
     state.button_a = check("A")
     state.button_b = check("B")
 
-    # second pass: assess state
+    # take action - change the state
     if state.button_a:
+        print("LED: on")
         state.led = True
     else:
         state.led = False
 
     if state.button_b:
+        print("RGB on. Color: {}".format(state.color))
         state.rgb = True
     else:
         state.rgb = False
+        state.random_color()
 
-    # third pass: reconcile state
+    # take action - do things that cause side effects
     led.value = state.led
 
     if state.rgb:
-        rgb.fill(state.color)
+        rgb[0] = state.color
     else:
-        rgb.fill((0, 0, 0))
+        rgb[0] = (0, 0, 0)
 
     time.sleep(0.2)
